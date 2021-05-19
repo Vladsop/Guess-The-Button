bad275559ea1393733bb5b715661555411bd737a
Vlad Soporan <73958706+Vladsop@users.noreply.github.com>
2021-05-20 00:19:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var winner = Math.floor(Math.random() * 3) + 1;
+$("#mixbuttons").hide();
+
+function reset() {
+    winner = Math.floor(Math.random() * 3) + 1;
+    $("#mixbuttons").hide();
+    return document.getElementById("message").innerHTML = "";
+
+}
+
+function check_button(id) {
+    if (winner.toString() === id) {
+        confetti({
+            particleCount: 500,
+            spread: 120,
+            origin: {
+                y: 0.6
+            }
+        });
+        $("#mixbuttons").show()
+        return document.getElementById("message").innerHTML = "Congratulations, You guessed it!"
+    }
+    $("#mixbuttons").show()
+    return document.getElementById("message").innerHTML = "Sorry, Would you like to try again?"
+}
